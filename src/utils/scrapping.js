8565e655514974fd743ed53db0cfa3af1a8560b0
Adson Luis <adson.vitorio@fatec.sp.gov.br>
2023-11-28 19:57:42 -0300

    Restruturando scrap

--- src/utils/scrapping.js
+++ src/utils/scrapping.js
@@ -3,38 +3,45 @@ const pup = require("puppeteer");
 const PROD = true;
 
 const urlBase = "https://www.fateconline.com.br/sistema";
-let initalError;
+let initalError = null;
 let userData = null;
-let browser;
-let state;
+let browser = null;
 
-async function initializePuppeteer() {
+async function Scrap({ email, password, type }) {
   try {
-    if (PROD === true) {
-      browser = await pup.launch({
-        headless: true,
-        args: [
-          "--disable-setuid-sandbox",
-          "--no-sandbox",
-          "--single-process",
-          "--no-zygote",
-          "--disable-features=site-per-process",
-        ],
-        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
-      });
-    } else {
-      browser = await pup.launch({ headless: false });
+    try {
+      if (PROD === true) {
+        browser = await pup.launch({
+          headless: true,
+          args: [
+            "--disable-setuid-sandbox",
+            "--no-sandbox",
+            "--single-process",
+            "--no-zygote",
+            "--disable-features=site-per-process",
+          ],
+          executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+        });
+      } else {
+        browser = await pup.launch({ headless: false });
+      }
+    } catch (err) {
+      console.log(err);
+      await browser.close();
+      throw new Error("Não foi possível iniciar o Puppeteer");
     }
 
-    return browser;
-  } catch (err) {
-    console.log(err);
-    return (initalError = "Não foi possível iniciar o Puppeteer");
-  }
-}
+    const page = await (await browser.pages())[0];
 
-async function Login(page, email, password) {
-  try {
+    if (!page) {
+      throw new Error("Página não inciou");
+    }
+
+    await page.goto(urlBase, {
+      waitUntil: "load",
+      timeout: 0,
+    });
+
     await page.waitForSelector("#txtEmail");
     await page.waitForSelector("#txtSenha");
     await page.waitForSelector("#btnLogar");
@@ -50,116 +57,8 @@ async function Login(page, email, password) {
       throw new Error("Login Incorreto");
     }
 
-    return "Sucesso";
-  } catch (err) {
-    console.log(err);
-    if (!initalError) {
-      return (initalError = "Não foi possível fazer o login");
-    } else {
-      return null;
-    }
-  }
-}
-
-async function ExtractData(page, email, password, type) {
-  try {
-    await page.waitForSelector(".dropdown-toggle");
-    await page.click(".dropdown-toggle");
-    await page.waitForSelector("#Li9");
-
-    await Promise.all([page.waitForNavigation(), page.click("#Li9")]);
-
-    if (page.url() !== `${urlBase}/Paginas/Alunos/Faltas.aspx`) {
-      throw new Error("O redirecionamento falhou");
-    }
-
-    await page.waitForSelector("#body_ddlEstrutura");
-    await page.waitForSelector(".sorting_1");
-    await page.waitForSelector("#ImgMiniatura");
-
-    let RA = await page.$$eval("#body_ddlEstrutura > option", (nRA) =>
-      nRA.map((option) => option.text)
-    );
-    RA = RA[1];
-    const selector = "td.sorting_1";
-    const discipline = await page.evaluate((selector) => {
-      const tds = document.querySelectorAll(selector);
-
-      return Array.from(tds, (td) => td.textContent.trim());
-    }, selector);
-
-    const photo = await page.$eval('img[id="ImgMiniatura"]', (img) => img.src);
-
-    await page.waitForSelector(".navbar-custom-menu");
-    await page.click(".navbar-custom-menu");
-    await page.waitForSelector(".btn");
-
-    await Promise.all([page.waitForNavigation(), page.click(".btn")]);
-
-    if (page.url() !== `${urlBase}/Paginas/Alunos/Cadastro.aspx`) {
-      throw new Error("O redirecionamento falhou");
-    }
-
-    await page.waitForSelector("#txbNome");
-
-    const name = await page.$eval(
-      'input[name="ctl00$body$txbNome"]',
-      (input) => input.value
-    );
-
-    const data = {
-      RA: RA.substr(5, 8).trim(),
-      name: name,
-      email: email,
-      password: password,
-      type: type,
-      photo: photo,
-      curse: RA.substr(22, 4).trim(),
-      periode: RA.substr(34, 6).trim(),
-      class: discipline,
-    };
-
-    return data;
-  } catch (err) {
-    console.log(err);
-    if (!initalError) {
-      return (initalError = "Não foi possível extrair os dados");
-    } else {
-      return null;
-    }
-  }
-}
-
-async function Scrap({ email, password, type }) {
-  try {
-    const browser = await initializePuppeteer();
-
-    if (
-      browser === null ||
-      initalError === "Não foi possível iniciar o Puppeteer"
-    ) {
-      throw new Error("Não foi possível iniciar o Puppeteer");
-    }
-
-    const page = await browser.newPage();
-
-    if (!page) {
-      throw new Error("Página não inciou");
-    }
-
-    await page.goto(urlBase, {
-      waitUntil: "load",
-      timeout: 0,
-    });
-
-    state = await Login(page, email, password);
-
     console.log(state);
     console.log(initalError);
-
-    if (state === null || initalError === "Não foi possível fazer o login") {
-      throw new Error("Não foi possível fazer o login");
-    }
 
     if (type === "aluno") {
       if (page.url() === `${urlBase}/Paginas/Professores/`) {
@@ -171,13 +70,69 @@ async function Scrap({ email, password, type }) {
       }
     }
 
-    userData = await ExtractData(page, email, password, type);
+    try {
+      await page.waitForSelector(".dropdown-toggle");
+      await page.click(".dropdown-toggle");
+      await page.waitForSelector("#Li9");
 
-    if (
-      userData === null ||
-      initalError === "Não foi possível extrair os dados"
-    ) {
-      throw new Error("Sem nenhum dado");
+      await Promise.all([page.waitForNavigation(), page.click("#Li9")]);
+
+      if (page.url() !== `${urlBase}/Paginas/Alunos/Faltas.aspx`) {
+        throw new Error("O redirecionamento falhou");
+      }
+
+      await page.waitForSelector("#body_ddlEstrutura");
+      await page.waitForSelector(".sorting_1");
+      await page.waitForSelector("#ImgMiniatura");
+
+      let RA = await page.$$eval("#body_ddlEstrutura > option", (nRA) =>
+        nRA.map((option) => option.text)
+      );
+      RA = RA[1];
+      const selector = "td.sorting_1";
+      const discipline = await page.evaluate((selector) => {
+        const tds = document.querySelectorAll(selector);
+
+        return Array.from(tds, (td) => td.textContent.trim());
+      }, selector);
+
+      const photo = await page.$eval(
+        'img[id="ImgMiniatura"]',
+        (img) => img.src
+      );
+
+      await page.waitForSelector(".navbar-custom-menu");
+      await page.click(".navbar-custom-menu");
+      await page.waitForSelector(".btn");
+
+      await Promise.all([page.waitForNavigation(), page.click(".btn")]);
+
+      if (page.url() !== `${urlBase}/Paginas/Alunos/Cadastro.aspx`) {
+        throw new Error("O redirecionamento falhou");
+      }
+
+      await page.waitForSelector("#txbNome");
+
+      const name = await page.$eval(
+        'input[name="ctl00$body$txbNome"]',
+        (input) => input.value
+      );
+
+      const data = {
+        RA: RA.substr(5, 8).trim(),
+        name: name,
+        email: email,
+        password: password,
+        type: type,
+        photo: photo,
+        curse: RA.substr(22, 4).trim(),
+        periode: RA.substr(34, 6).trim(),
+        class: discipline,
+      };
+    } catch (err) {
+      console.log(err);
+      await browser.close();
+      throw new Error("Não foi possível extrair");
     }
 
     await browser.close();
